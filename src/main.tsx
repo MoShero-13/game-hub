17f0a82cbe6b4767eecfb1d8d3ea309163b99159
MoShero-13 <mo3206213@gmail.com>
2024-11-11 10:40:19 +0300

    Setting up ReactQuery

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "./components/ui/provider";
 import { ColorModeProvider } from "./components/ui/color-mode";
 import App from "./App";
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Provider>
-      <ColorModeProvider>
-        <App />
-      </ColorModeProvider>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider>
+        <ColorModeProvider>
+          <App />
+        </ColorModeProvider>
+      </Provider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
