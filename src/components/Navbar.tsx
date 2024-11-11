b8be7ca9553d9ff949eb770bc8975fc6d5b54981
MoShero-13 <mo3206213@gmail.com>
2024-11-11 15:16:54 +0300

    Changing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import { ColorModeButton } from "./ui/color-mode";
 import SearchInput from "./SearchInput";
+import React from "react";
 
 interface Props {
   onSearch: (searchText: string) => void;
