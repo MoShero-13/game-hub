a664857ff5a7c8ddc3774b5a74f8423abd259d63
MoShero-13 <mo3206213@gmail.com>
2025-01-27 11:43:58 +0300

    Build expandable text

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,3 +1,4 @@
+import ExpandableText from "@/components/ExpandableText";
 import useGame from "@/hooks/useGame";
 import { Heading, Spinner, Text } from "@chakra-ui/react";
 import React from "react";
@@ -14,7 +15,7 @@ const GameDetailPage = () => {
   return (
     <>
       <Heading>{game?.name}</Heading>
-      <Text>{game?.description_raw}</Text>
+      <ExpandableText>{game?.description_raw}</ExpandableText>
     </>
   );
 };
